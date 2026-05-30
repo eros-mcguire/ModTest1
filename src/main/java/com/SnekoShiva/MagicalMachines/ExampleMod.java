7c2d624e4c93530b82a003a8163147c4b964b8f9
Grayson-Banta <draygon6@gmail.com>
2026-05-30 14:30:38 -0700

    Test

--- src/main/java/com/SnekoShiva/MagicalMachines/ExampleMod.java
+++ src/main/java/com/SnekoShiva/MagicalMachines/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.SnekoShiva.MagicalMachines;
 
 import org.slf4j.Logger;
 
--- src/main/java/com/SnekoShiva/MagicalMachines/ExampleModClient.java
+++ src/main/java/com/SnekoShiva/MagicalMachines/ExampleModClient.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.SnekoShiva.MagicalMachines;
 
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
